18cf11bab23b1c9957bc51daa42885726655d449
Larissa de Souza Mendes <larissa.souza.mendes@escola.pr.gov.br>
2025-06-13 10:33:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+let cobra =[{x:10, y:10}];
+let comida = gerarComida();
+let direção = 'DIREITA';
+let velocidade = 150;
+let pontos = 0;
+let intervalo;
+
+function iniciar(){
+    criarTabuleiro();
+    document.addEventListener('Keydown', mudarDirecao);
+    intervalo = setInterval(moverCobra, velocidade);
+}
+function criarTabuleiro(){
+    const tabuleiro = document.getElementById('game-board');
+tabuleiro.innerHTML = '';
+    
+cobra.forEach(segmento => {
+    const elemento = document.createElement('div');
+    elemento.style.gridRowStart = segmento.y;
+    elemento.style.gridColumnStart = segmento.x;
+    elemento.classList.add('snake');
+    tabuleiro.appendChild(elemento);
+})
+function gerarComida(){
+    let novaComida;
+    while (!novaComida || posicaoOcupada(novaComida))
+    {
+        novaComida = {
+            x:Math.floor(Math.random()*18)+2,
+            y:Math.floor(Math.random()*18)+2
+        };
+    }
+}
+}
