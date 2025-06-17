b301f59dd05833c6fb9e6c2cc7925ae98063eb7b
Larissa de Souza Mendes <larissa.souza.mendes@escola.pr.gov.br>
2025-06-17 09:31:56 -0300

    aa

--- main.js
+++ main.js
@@ -20,7 +20,13 @@ cobra.forEach(segmento => {
     elemento.style.gridColumnStart = segmento.x;
     elemento.classList.add('snake');
     tabuleiro.appendChild(elemento);
-})
+});
+const elementoComida = document.createElement('div');
+elementoComida.style.gridRowStart = comida.y;
+elementoComida.style.gridColumnStart = comida.x;
+elementoComida.classList.add('food');
+tabuleiro.appendChild(elementoComida);
+}
 function gerarComida(){
     let novaComida;
     while (!novaComida || posicaoOcupada(novaComida))
@@ -30,5 +36,21 @@ function gerarComida(){
             y:Math.floor(Math.random()*18)+2
         };
     }
+    return novaComida;
+}
+function posicaoOcupada(posicao){
+    return cobra.some(segmento=> segmento.x === posicao.x && segmento.y===posicao.y);
+}
+function mudarDirecao(event){
+    const tecla = event.key;
+if (tecla === "ArrowUp" && direcao !== 'BAIXO'){
+    direcao = 'CIMA';
+} else if(tecla === 'ArrowDown'&& direcao !== 'CIMA')
+{
+    direcao = 'BAIXO';
+} else if(tecla === 'ArrowLeft' && direcao !== 'DIREITA'){
+    direcao = 'ESQUERDA';
+} else if (tecla === 'ArrowRight' && direcao !== 'ESQUERDA'){
+    direcao = 'DIREITA';
 }
 }
